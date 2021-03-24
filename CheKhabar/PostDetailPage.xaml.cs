555b977cc492f75a7d471b3602176b94dccec24f
Majid Darvishan <majiddarvishan@outlook.com>
2021-03-25 04:09:34 +0430

    Rename project

---
+++ CheKhabar/PostDetailPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using CheKhabar.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheKhabar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostDetailPage : ContentPage
+    {
+        Post selectedPost;
+        public PostDetailPage(Post selectedPost)
+        {
+            InitializeComponent();
+
+            this.selectedPost = selectedPost;
+            experiencedEntry.Text = selectedPost.Experience;
+        }
+
+        private void updateButton_Clicked(object sender, EventArgs e)
+        {
+            selectedPost.Experience = experiencedEntry.Text;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Update(selectedPost);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience successfully updated", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Experience failed to be updated", "OK");
+                }
+            }
+        }
+
+        private void deleteButton_Clicked(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Delete(selectedPost);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience successfully deleted", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Experience failed to be deleted", "OK");
+                }
+            }
+        }
+    }
+}
