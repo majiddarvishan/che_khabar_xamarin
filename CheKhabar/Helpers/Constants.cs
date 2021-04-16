be73bc111cc8460f9204c774d5c84d13c4a54998
Majid Darvishan <majiddarvishan@outlook.com>
2021-04-16 23:41:26 +0430

    Fix https

--- CheKhabar/Helpers/Constants.cs
+++ CheKhabar/Helpers/Constants.cs
@@ -6,11 +6,12 @@ namespace CheKhabar.Helpers
 {
     class Constants
     {
+        //"http://192.168.1.172:5000";
+        public const string BASE_URL = "https://majiddarvishan.pythonanywhere.com"; 
+
         public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
         public const string CLIENT_ID = "NK2FI4P5YIEEXAPXSLATSSO03IU40VQVJ0EEWS15JPTPLSKH";
         public const string CLIENT_SECRET = "OQXXY1T2PRDNRBX3B3DP0CR2TPU2HJ5QTO3SX5CT2AOW1IU0";
-
-        public const string BASE_URL = "http://192.168.1.172:5000";
 
         public const string ADD_ADVERTISEMENT = BASE_URL + "/advertisements";
         public const string ADVERTISEMENT_SEARCH = BASE_URL + "/advertisements?&lat={0}&lng={1}&distance={2}";
--- CheKhabar/NewTravelPage.xaml.cs
+++ CheKhabar/NewTravelPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using CheKhabar.Logic;
+﻿using CheKhabar.Helpers;
+using CheKhabar.Logic;
 using CheKhabar.Model;
+using Newtonsoft.Json;
 using Plugin.Geolocator;
 using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,34 +40,37 @@ namespace CheKhabar
             try
             {
                 var selectedVenue = venueListView.SelectedItem as Venue;
-                var firstCategory = selectedVenue.categories.FirstOrDefault();
 
-                Post post = new Post()
-                {
-                    Experience = experienceEntry.Text,
-                    CategoryId = firstCategory.id,
-                    CategoryName = firstCategory.name,
-                    Address = selectedVenue.location.address,
-                    Distance = selectedVenue.location.distance,
-                    Latitude = selectedVenue.location.lat,
-                    Longitude = selectedVenue.location.lng,
-                    VenueName = selectedVenue.name
-                };
+                SaveVenue(selectedVenue);
 
-                using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-                {
-                    conn.CreateTable<Post>();
-                    int rows = conn.Insert(post);
+                //var firstCategory = selectedVenue.categories.FirstOrDefault();
 
-                    if (rows > 0)
-                    {
-                        DisplayAlert("Success", "Experience successfully inserted", "OK");
-                    }
-                    else
-                    {
-                        DisplayAlert("Failure", "Experience failed to be inserted", "OK");
-                    }
-                }
+                //Post post = new Post()
+                //{
+                //    Experience = experienceEntry.Text,
+                //    CategoryId = firstCategory.id,
+                //    CategoryName = firstCategory.name,
+                //    Address = selectedVenue.location.address,
+                //    Distance = selectedVenue.location.distance,
+                //    Latitude = selectedVenue.location.lat,
+                //    Longitude = selectedVenue.location.lng,
+                //    VenueName = selectedVenue.name
+                //};
+
+                //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                //{
+                //    conn.CreateTable<Post>();
+                //    int rows = conn.Insert(post);
+
+                //    if (rows > 0)
+                //    {
+                //        DisplayAlert("Success", "Experience successfully inserted", "OK");
+                //    }
+                //    else
+                //    {
+                //        DisplayAlert("Failure", "Experience failed to be inserted", "OK");
+                //    }
+                //}
             }
             catch(NullReferenceException ex)
             {
@@ -73,6 +79,65 @@ namespace CheKhabar
             catch(Exception ex)
             {
 
+            }
+        }
+
+        private async void SaveVenue(Venue venue)
+        {
+            //var description = experienceEntry.Text;
+            //if (string.IsNullOrEmpty(description))
+            //{
+            //    _ = DisplayAlert("Error", "You must enter description", "OK");
+            //}
+            //else
+            {
+                Advertisement adv = new Advertisement()
+                {
+                    mobile = App.LoginUserNumber,
+                    description = venue.name,
+                    start_time = DateTime.Now.ToString("yyyy-MM-dd"),
+                    end_time = "",
+                    latitude = venue.location.lat,
+                    longitude = venue.location.lng,
+                    tags = venue.categories.FirstOrDefault().name
+                };
+
+                var url = Constants.ADD_ADVERTISEMENT;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var json = JsonConvert.SerializeObject(adv);
+                    var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync(url, stringContent);
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        SaveToDatabase(adv);
+                        _ = Navigation.PushAsync(new HomePage());
+                    }
+                    else
+                    {
+                        _ = DisplayAlert("Error", "Problem in connecting with server, please try later", "OK");
+                    }
+                }
+            }
+        }
+
+        private void SaveToDatabase(Advertisement adv)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Advertisement>();
+                int rows = conn.Insert(adv);
+
+                //if (rows > 0)
+                //{
+                //    DisplayAlert("Success", "Advertisement successfully inserted", "OK");
+                //}
+                //else
+                //{
+                //    DisplayAlert("Failure", "Advertisement failed to be inserted", "OK");
+                //}
             }
         }
     }
