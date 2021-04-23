84d0a23f7de23f57b7ef9d6073c5a9b824c8d62b
Majid Darvishan <majiddarvishan@outlook.com>
2021-04-23 22:53:44 +0430

    Improve HistoryPage UI

--- CheKhabar/HistoryPage.xaml.cs
+++ CheKhabar/HistoryPage.xaml.cs
@@ -3,6 +3,7 @@ using CheKhabar.Model;
 using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,6 +49,41 @@ namespace CheKhabar
             {
                 Navigation.PushAsync(new PostDetailPage(selectedAdv));
             }
+        }
+    }
+
+    //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/data-binding/converters
+    public class GregorianToSolarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //http://www.w3-farsi.com/convert-gregorian-date-to-solar-csharp/
+            if (((String)value).Length != 0)
+            {
+                DateTime dateTime = ConvertStrToDate((String)value);
+
+                PersianCalendar persianCalendar = new PersianCalendar();
+                return string.Format(@"{0}/{1}/{2}",
+                     persianCalendar.GetYear(dateTime),
+                     persianCalendar.GetMonth(dateTime),
+                     persianCalendar.GetDayOfMonth(dateTime));
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+
+        private DateTime ConvertStrToDate(String inputDate)
+        {
+            string[] dateFormats = new[] { "yyyy-MM-dd"};
+            CultureInfo provider = new CultureInfo("en-US");
+            DateTime outDate = DateTime.ParseExact(inputDate, dateFormats, provider,DateTimeStyles.AdjustToUniversal);
+
+            return outDate;
         }
     }
 }
